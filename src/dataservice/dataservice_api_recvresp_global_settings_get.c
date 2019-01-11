d003c0e16503704b2d571eab0f18d5dac7f3ca5a
Justin Handville <nanolith@gmail.com>
2019-01-10 22:16:47 -0500

    Cleaned up function documentation.

--- src/dataservice/dataservice_api_recvresp_global_settings_get.c
+++ src/dataservice/dataservice_api_recvresp_global_settings_get.c
@@ -33,12 +33,37 @@
  * success, the data value and size are both updated to reflect the data read
  * from the query.
  *
- * A status code of 2 represents a "not found" error code.  In future versions
- * of this API, this will be updated to a enumerated value.
+ * If the status code is updated with an error from the service, then this error
+ * will be reflected in the status variable, and a AGENTD_STATUS_SUCCESS will be
+ * returned by this function.  Thus, both the return value of this function and
+ * the upstream status code must be checked for correct operation.  Here are a
+ * few possible status codes; it is not possible to list them all.
+ *      - AGENTD_STATUS_SUCCESS if the remote operation completed successfully.
+ *      - AGENTD_ERROR_DATASERVICE_NOT_FOUND if the requested data was not
+ *        found.
+ *      - AGENTD_ERROR_DATASERVICE_NOT_AUTHORIZED if this client node is not
+ *        authorized to perform the requested operation.
+ *      - AGENTD_ERROR_DATASERVICE_REQUEST_PACKET_INVALID_SIZE if the request
+ *        packet size is invalid.
+ *      - AGENTD_ERROR_DATASERVICE_CHILD_CONTEXT_BAD_INDEX if the child context
+ *        index is out of bounds.
+ *      - AGENTD_ERROR_DATASERVICE_CHILD_CONTEXT_INVALID if the child context is
+ *        invalid.
  *
- * \returns 0 if the response was read successfully, IPC_ERROR_CODE_WOULD_BLOCK
- * if the response cannot yet be read, and non-zero if the response could not be
- * successfully read.
+ * \returns a status code indicating success or failure.
+ *      - AGENTD_STATUS_SUCCESS on success.
+ *      - AGENTD_ERROR_IPC_WOULD_BLOCK if the operation was halted because it
+ *        would block this thread.
+ *      - AGENTD_ERROR_DATASERVICE_IPC_READ_DATA_FAILURE if reading data from
+ *        the socket failed.
+ *      - AGENTD_ERROR_DATASERVICE_RECVRESP_UNEXPECTED_DATA_PACKET_SIZE if the
+ *        data packet size is unexpected.
+ *      - AGENTD_ERROR_DATASERVICE_RECVRESP_UNEXPECTED_METHOD_CODE if the
+ *        method code was unexpected.
+ *      - AGENTD_ERROR_DATASERVICE_RECVRESP_MALFORMED_PAYLOAD_DATA if the
+ *        payload data was malformed.
+ *      - AGENTD_ERROR_GENERAL_OUT_OF_MEMORY if this operation encountered an
+ *        out-of-memory error.
  */
 int dataservice_api_recvresp_global_settings_get(
     ipc_socket_context_t* sock, uint32_t* offset, uint32_t* status, void* data,
