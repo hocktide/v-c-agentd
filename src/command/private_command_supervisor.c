baec91786fcc86aef5d1c2d7ca2b3316b1a373ac
Justin Handville <nanolith@gmail.com>
2019-03-04 23:33:25 -0500

    Flesh out supervisor service.

--- src/command/private_command_supervisor.c
+++ src/command/private_command_supervisor.c
@@ -1,53 +1,144 @@
 /**
  * \file command/private_command_supervisor.c
  *
- * \brief Be the supervisor that spawns everyone else.
+ * \brief Create, spawn, and introduce all services.
  *
- * \copyright 2018 Velo Payments, Inc.  All rights reserved.
+ * \copyright 2018-2019 Velo Payments, Inc.  All rights reserved.
  */
 
+#include <agentd/status_codes.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/wait.h>
 
-static void private_signal_handler_supervisor(int signal);
+/* forward decls. */
+static int sighandler_install();
+static void sighandler_uninstall();
+static void supervisor_signal_handler(int signal);
+static int supervisor_run();
+
+/* flag to indicate whether we should continue running. */
+static bool keep_running = false;
 
 /**
  * \brief Run the the supervisor.
  */
 void private_command_supervisor()
 {
-    /*
-     * This one is special as the application is terminated at the exit of this handler.
-     * We'll want to make some sort of a notification here.
-     */
-
-    signal(SIGHUP, private_signal_handler_supervisor);
-    signal(SIGKILL, private_signal_handler_supervisor);
-    signal(SIGTERM, private_signal_handler_supervisor);
-    signal(SIGCHLD, private_signal_handler_supervisor);
-
-    for (;;)
+    /* install the signal handlers. */
+    if (AGENTD_STATUS_SUCCESS != sighandler_install())
     {
-        wait(NULL);
+        perror("sighandler_install");
+        return;
     }
+
+    /* we are in the running state. */
+    keep_running = true;
+
+    /* TODO - set the process name. */
+
+    while (keep_running)
+    {
+        /* if supervisor_run fails, exit. */
+        if (AGENTD_STATUS_SUCCESS != supervisor_run())
+        {
+            keep_running = false;
+        }
+    }
+
+    /* uninstall the signal handlers on exit. */
+    sighandler_uninstall();
 }
 
-static void private_signal_handler_supervisor(int signal)
+/**
+ * \brief Run the supervisor.
+ *
+ * This function attempts to bootstrap all child services and then waits until
+ * an appropriate signal is detected prior to exiting.
+ *
+ * The bootstrap process first reads the configuration file and then uses this
+ * configuration file to start each child service.
+ *
+ * \returns a status code indicating success or failure.
+ *          - AGENTD_STATUS_SUCCESS on success.
+ */
+static int supervisor_run()
 {
-    /*
-     * We'll want to wait on the next child pid here,
-     * or bubble this up to the loop above to wait on the proc.
-     */
+    int retval = AGENTD_STATUS_SUCCESS;
+
+    /* read config. */
+    /* create listener service. */
+    /* create data service for protocol service. */
+    /* create protocol service. */
+    /* create data service for consensus service. */
+    /* create consensus service. */
+
+    sleep(10);
+
+    return retval;
+}
+
+/**
+ * \brief Install signal handlers.
+ *
+ * \returns a status code indicating success or failure.
+ *          - AGENTD_STATUS_SUCCESS on success.
+ *          - AGENTD_ERROR_SUPERVISOR_SIGNAL_INSTALLATION if installation of a
+ *            signal handler failed.
+ */
+static int sighandler_install()
+{
+    /* attempt to catch SIGHUP signal. */
+    if (SIG_ERR == signal(SIGHUP, &supervisor_signal_handler))
+    {
+        return AGENTD_ERROR_SUPERVISOR_SIGNAL_INSTALLATION;
+    }
+
+    /* attempt to catch SIGTERM signal. */
+    if (SIG_ERR == signal(SIGTERM, &supervisor_signal_handler))
+    {
+        return AGENTD_ERROR_SUPERVISOR_SIGNAL_INSTALLATION;
+    }
+
+    /* attempt to catch SIGCHLD signal. */
+    if (SIG_ERR == signal(SIGCHLD, &supervisor_signal_handler))
+    {
+        return AGENTD_ERROR_SUPERVISOR_SIGNAL_INSTALLATION;
+    }
+
+    /* success */
+    return AGENTD_STATUS_SUCCESS;
+}
+
+/**
+ * \brief Uninstall signal handlers.
+ */
+static void sighandler_uninstall()
+{
+    /* since we are on the way out, failure doesn't matter. */
+    signal(SIGHUP, SIG_DFL);
+    signal(SIGTERM, SIG_DFL);
+    signal(SIGCHLD, SIG_DFL);
+}
+
+/**
+ * \brief Signal handler for the supervisor process.
+ */
+static void supervisor_signal_handler(int signal)
+{
     switch (signal)
     {
-        case SIGKILL:
-            break;
-        case SIGTERM:
-            break;
         case SIGCHLD:
-            break;
         case SIGHUP:
+            /* restart */
+            break;
+
+        case SIGTERM:
+        default:
+            /* attempt to gracefully shut down the process. */
+            keep_running = false;
             break;
     }
 }
