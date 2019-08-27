fb6f664eb669046d3088a6e74576eb182876a36f
Justin Handville <nanolith@gmail.com>
2019-08-27 04:01:05 -0400

    Model checks for decode methods.

---
+++ models/dataservice_decode_request_child_context_create_main.c
@@ -0,0 +1,25 @@
+#include <agentd/status_codes.h>
+#include <cbmc/model_assert.h>
+#include "../src/dataservice/dataservice_protocol_internal.h"
+
+/* nondeterministic size. */
+uint8_t nondet_size();
+
+int main(int argc, char* argv[])
+{
+    dataservice_request_child_context_create_t dreq;
+    size_t size = nondet_size();
+
+    const void* req = (const void*)malloc(size);
+    if (NULL == req)
+        return 0;
+
+    int retval =
+        dataservice_decode_request_child_context_create(req, size, &dreq);
+    if (AGENTD_STATUS_SUCCESS == retval)
+        dispose((disposable_t*)&dreq);
+
+    free(req);
+
+    return 0;
+}
