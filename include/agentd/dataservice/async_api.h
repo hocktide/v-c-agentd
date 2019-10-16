a93389aa31081edef7a12b746d82926052deeab4
Justin Handville <nanolith@gmail.com>
2019-10-15 22:23:45 -0400

    Extracted decoder for root_context_reduce_caps.

--- include/agentd/dataservice/async_api.h
+++ include/agentd/dataservice/async_api.h
@@ -37,6 +37,14 @@ typedef struct dataservice_response_root_context_init
 } dataservice_response_root_context_init_t;
 
 /**
+ * \brief Root Context Reduce Caps Response.
+ */
+typedef struct dataservice_response_root_context_reduce_caps
+{
+    dataservice_response_header_t hdr;
+} dataservice_response_root_context_reduce_caps_t;
+
+/**
  * \brief The memset disposer simply clears the data structure when disposed.
  *
  * \param disposable    The disposable to clear.
@@ -61,6 +69,25 @@ void dataservice_decode_response_memset_disposer(void* disposable);
 int dataservice_decode_response_root_context_init(
     const void* resp, size_t size,
     dataservice_response_root_context_init_t* dresp);
+
+/**
+ * \brief Decode a response from the root context reduce capabilities call.
+ *
+ * \param resp          The response payload to parse.
+ * \param size          The size of this response payload.
+ * \param dresp         The decoded response structure into which this response
+ *                      is decoded.
+ *
+ * \returns a status code indicating success or failure.
+ *      - AGENTD_STATUS_SUCCESS on success.
+ *      - AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE if the response
+ *        packet payload size is incorrect.
+ *      - AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER if one of the
+ *        parameters to the function is invalid.
+ */
+int dataservice_decode_response_root_context_reduce_caps(
+    const void* resp, size_t size,
+    dataservice_response_root_context_reduce_caps_t* dresp);
 
 /* make this header C++ friendly. */
 #ifdef __cplusplus
--- test/dataservice/test_dataservice_decode.cpp
+++ test/dataservice/test_dataservice_decode.cpp
@@ -57,9 +57,32 @@ TEST(dataservice_decode_test, response_root_context_init_null_checks)
 }
 
 /**
+ * Test that a response packet with an invalid method code returns an error.
+ */
+TEST(dataservice_decode_test, response_root_context_init_bad_method_code)
+{
+    uint8_t resp[12] = {
+        /* bad method code. */
+        0x80, 0x00, 0x00, 0x00,
+
+        /* offset == 1023 */
+        0x00, 0x00, 0x03, 0xFF,
+
+        /* status == 0x12345678 */
+        0x12, 0x34, 0x56, 0x78
+    };
+    dataservice_response_root_context_init_t dresp;
+
+    /* a valid response is successfully decoded. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RECVRESP_UNEXPECTED_METHOD_CODE,
+        dataservice_decode_response_root_context_init(
+            resp, sizeof(resp), &dresp));
+}
+
+/**
  * Test that a response packet is successfully decoded.
  */
-TEST(dataservice_decode_test, response_root_context_decoded)
+TEST(dataservice_decode_test, response_root_context_init_decoded)
 {
     uint8_t resp[12] = {
         /* method code. */
@@ -83,6 +106,108 @@ TEST(dataservice_decode_test, response_root_context_decoded)
         dresp.hdr.hdr.dispose);
     /* the method code is correct. */
     ASSERT_EQ(DATASERVICE_API_METHOD_LL_ROOT_CONTEXT_CREATE,
+        dresp.hdr.method_code);
+    /* the offset is correct. */
+    ASSERT_EQ(1023U, dresp.hdr.offset);
+    /* the status is correct. */
+    ASSERT_EQ(0x12345678U, dresp.hdr.status);
+    /* the payload size is correct. */
+    ASSERT_EQ(0U, dresp.hdr.payload_size);
+}
+
+/**
+ * Test that we check for sizes when decoding.
+ */
+TEST(dataservice_decode_test, response_root_context_reduce_caps_bad_sizes)
+{
+    uint8_t resp[100] = { 0 };
+    dataservice_response_root_context_reduce_caps_t dresp;
+
+    /* a zero size is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE,
+        dataservice_decode_response_root_context_reduce_caps(
+            resp, 0, &dresp));
+
+    /* a truncated size is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE,
+        dataservice_decode_response_root_context_reduce_caps(
+            resp, 2 * sizeof(uint32_t), &dresp));
+
+    /* a "too large" size is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE,
+        dataservice_decode_response_root_context_reduce_caps(
+            resp, 4 * sizeof(uint32_t), &dresp));
+}
+
+/**
+ * Test that we perform null checks in the decode.
+ */
+TEST(dataservice_decode_test, response_root_context_reduce_caps_null_checks)
+{
+    uint8_t resp[100] = { 0 };
+    dataservice_response_root_context_reduce_caps_t dresp;
+
+    /* a null response packet pointer is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER,
+        dataservice_decode_response_root_context_reduce_caps(
+            nullptr, 3 * sizeof(uint32_t), &dresp));
+
+    /* a null decoded response structure pointer is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER,
+        dataservice_decode_response_root_context_reduce_caps(
+            resp, 3 * sizeof(uint32_t), nullptr));
+}
+
+/**
+ * Test that a response packet with an invalid method code returns an error.
+ */
+TEST(dataservice_decode_test, response_root_context_reduce_caps_bad_method_code)
+{
+    uint8_t resp[12] = {
+        /* bad method code. */
+        0x80, 0x00, 0x00, 0x00,
+
+        /* offset == 1023 */
+        0x00, 0x00, 0x03, 0xFF,
+
+        /* status == 0x12345678 */
+        0x12, 0x34, 0x56, 0x78
+    };
+    dataservice_response_root_context_reduce_caps_t dresp;
+
+    /* a valid response is successfully decoded. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RECVRESP_UNEXPECTED_METHOD_CODE,
+        dataservice_decode_response_root_context_reduce_caps(
+            resp, sizeof(resp), &dresp));
+}
+
+/**
+ * Test that a response packet is successfully decoded.
+ */
+TEST(dataservice_decode_test, response_root_context_reduce_caps_decoded)
+{
+    uint8_t resp[12] = {
+        /* method code. */
+        0x00, 0x00, 0x00, 0x01,
+
+        /* offset == 1023 */
+        0x00, 0x00, 0x03, 0xFF,
+
+        /* status == 0x12345678 */
+        0x12, 0x34, 0x56, 0x78
+    };
+    dataservice_response_root_context_reduce_caps_t dresp;
+
+    /* a valid response is successfully decoded. */
+    ASSERT_EQ(AGENTD_STATUS_SUCCESS,
+        dataservice_decode_response_root_context_reduce_caps(
+            resp, sizeof(resp), &dresp));
+
+    /* the disposer is set to the memset disposer. */
+    ASSERT_EQ(&dataservice_decode_response_memset_disposer,
+        dresp.hdr.hdr.dispose);
+    /* the method code is correct. */
+    ASSERT_EQ(DATASERVICE_API_METHOD_LL_ROOT_CONTEXT_REDUCE_CAPS,
         dresp.hdr.method_code);
     /* the offset is correct. */
     ASSERT_EQ(1023U, dresp.hdr.offset);
