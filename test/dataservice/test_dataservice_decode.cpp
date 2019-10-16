3ecd48d34846d227aa82f9d89bffed9c2b96e48a
Justin Handville <nanolith@gmail.com>
2019-10-15 23:13:13 -0400

    Extracted decoder for child_context_create.

--- test/dataservice/test_dataservice_decode.cpp
+++ test/dataservice/test_dataservice_decode.cpp
@@ -216,3 +216,114 @@ TEST(dataservice_decode_test, response_root_context_reduce_caps_decoded)
     /* the payload size is correct. */
     ASSERT_EQ(0U, dresp.hdr.payload_size);
 }
+
+/**
+ * Test that we check for sizes when decoding.
+ */
+TEST(dataservice_decode_test, response_child_context_create_bad_sizes)
+{
+    uint8_t resp[100] = {
+        /* method code. */
+        0x00, 0x00, 0x00, 0x02,
+
+        /* offset == 1023 */
+        0x00, 0x00, 0x03, 0xFF,
+
+        /* status == 0x00000000 */
+        0x00, 0x00, 0x00, 0x00
+    };
+    dataservice_response_child_context_create_t dresp;
+
+    /* a zero size is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE,
+        dataservice_decode_response_child_context_create(
+            resp, 0, &dresp));
+
+    /* a truncated size is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RECVRESP_UNEXPECTED_DATA_PACKET_SIZE,
+        dataservice_decode_response_child_context_create(
+            resp, 3 * sizeof(uint32_t), &dresp));
+}
+
+/**
+ * Test that we perform null checks in the decode.
+ */
+TEST(dataservice_decode_test, response_child_context_create_null_checks)
+{
+    uint8_t resp[100] = { 0 };
+    dataservice_response_child_context_create_t dresp;
+
+    /* a null response packet pointer is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER,
+        dataservice_decode_response_child_context_create(
+            nullptr, 3 * sizeof(uint32_t), &dresp));
+
+    /* a null decoded response structure pointer is invalid. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER,
+        dataservice_decode_response_child_context_create(
+            resp, 3 * sizeof(uint32_t), nullptr));
+}
+
+/**
+ * Test that a response packet with an invalid method code returns an error.
+ */
+TEST(dataservice_decode_test, response_child_context_create_bad_method_code)
+{
+    uint8_t resp[12] = {
+        /* bad method code. */
+        0x80, 0x00, 0x00, 0x00,
+
+        /* offset == 1023 */
+        0x00, 0x00, 0x03, 0xFF,
+
+        /* status == 0x12345678 */
+        0x12, 0x34, 0x56, 0x78
+    };
+    dataservice_response_child_context_create_t dresp;
+
+    /* a valid response is successfully decoded. */
+    ASSERT_EQ(AGENTD_ERROR_DATASERVICE_RECVRESP_UNEXPECTED_METHOD_CODE,
+        dataservice_decode_response_child_context_create(
+            resp, sizeof(resp), &dresp));
+}
+
+/**
+ * Test that a response packet is successfully decoded.
+ */
+TEST(dataservice_decode_test, response_child_context_create_decoded)
+{
+    uint8_t resp[16] = {
+        /* method code. */
+        0x00, 0x00, 0x00, 0x02,
+
+        /* offset == 1023 */
+        0x00, 0x00, 0x03, 0xFF,
+
+        /* status == 0x00000000 */
+        0x00, 0x00, 0x00, 0x00,
+
+        /* child index == 0x12345678 */
+        0x12, 0x34, 0x56, 0x78
+    };
+    dataservice_response_child_context_create_t dresp;
+
+    /* a valid response is successfully decoded. */
+    ASSERT_EQ(AGENTD_STATUS_SUCCESS,
+        dataservice_decode_response_child_context_create(
+            resp, sizeof(resp), &dresp));
+
+    /* the disposer is set to the memset disposer. */
+    ASSERT_EQ(&dataservice_decode_response_memset_disposer,
+        dresp.hdr.hdr.dispose);
+    /* the method code is correct. */
+    ASSERT_EQ(DATASERVICE_API_METHOD_LL_CHILD_CONTEXT_CREATE,
+        dresp.hdr.method_code);
+    /* the offset is correct. */
+    ASSERT_EQ(1023U, dresp.hdr.offset);
+    /* the status code is correct. */
+    ASSERT_EQ(AGENTD_STATUS_SUCCESS, (int)dresp.hdr.status);
+    /* the child index is correct. */
+    ASSERT_EQ(0x12345678U, dresp.child);
+    /* the payload size is correct. */
+    ASSERT_EQ(sizeof(dresp) - sizeof(dresp.hdr), dresp.hdr.payload_size);
+}
