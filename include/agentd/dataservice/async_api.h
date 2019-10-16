43e1469838dbb291eb6296a5a22e3fe82af3a046
Justin Handville <nanolith@gmail.com>
2019-10-16 03:40:19 -0400

    Extracted decoder for transaction_get_first.

--- include/agentd/dataservice/async_api.h
+++ include/agentd/dataservice/async_api.h
@@ -68,7 +68,7 @@ typedef struct dataservice_response_child_context_close
 typedef struct dataservice_response_global_settings_get
 {
     dataservice_response_header_t hdr;
-    void* data;
+    const void* data;
     size_t data_size;
 } dataservice_response_global_settings_get_t;
 
@@ -87,6 +87,17 @@ typedef struct dataservice_response_transaction_submit
 {
     dataservice_response_header_t hdr;
 } dataservice_response_transaction_submit_t;
+
+/**
+ * \brief Transaction Get First Response.
+ */
+typedef struct dataservice_response_transaction_get_first
+{
+    dataservice_response_header_t hdr;
+    data_transaction_node_t node;
+    const void* data;
+    size_t data_size;
+} dataservice_response_transaction_get_first_t;
 
 /**
  * \brief The memset disposer simply clears the data structure when disposed.
@@ -226,6 +237,25 @@ int dataservice_decode_response_global_settings_set(
 int dataservice_decode_response_transaction_submit(
     const void* resp, size_t size,
     dataservice_response_transaction_submit_t* dresp);
+
+/**
+ * \brief Decode a response from the get first transaction query.
+ *
+ * \param resp          The response payload to parse.
+ * \param size          The size of this response payload.
+ * \param dresp         The decoded response structure into which this response
+ *                      is decoded.
+ *
+ * \returns a status code indicating success or failure.
+ *      - AGENTD_STATUS_SUCCESS on success.
+ *      - AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE if the response
+ *        packet payload size is incorrect.
+ *      - AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER if one of the
+ *        parameters to the function is invalid.
+ */
+int dataservice_decode_response_transaction_get_first(
+    const void* resp, size_t size,
+    dataservice_response_transaction_get_first_t* dresp);
 
 /* make this header C++ friendly. */
 #ifdef __cplusplus
