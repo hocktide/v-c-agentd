416d34c70fdedcb57f681d52fbb9d89c34c6eada
Justin Handville <nanolith@gmail.com>
2019-10-17 01:28:41 -0400

    Extracted decoder for block_get.

--- include/agentd/dataservice/async_api.h
+++ include/agentd/dataservice/async_api.h
@@ -165,6 +165,17 @@ typedef struct dataservice_response_artifact_get
 } dataservice_response_artifact_get_t;
 
 /**
+ * \brief Block Get Response.
+ */
+typedef struct dataservice_response_block_get
+{
+    dataservice_response_header_t hdr;
+    data_block_node_t node;
+    const void* data;
+    size_t data_size;
+} dataservice_response_block_get_t;
+
+/**
  * \brief The memset disposer simply clears the data structure when disposed.
  *
  * \param disposable    The disposable to clear.
@@ -454,6 +465,25 @@ int dataservice_decode_response_latest_block_id_get(
 int dataservice_decode_response_artifact_get(
     const void* resp, size_t size,
     dataservice_response_artifact_get_t* dresp);
+
+/**
+ * \brief Decode a response from the get block query.
+ *
+ * \param resp          The response payload to parse.
+ * \param size          The size of this response payload.
+ * \param dresp         The decoded response structure into which this response
+ *                      is decoded.
+ *
+ * \returns a status code indicating success or failure.
+ *      - AGENTD_STATUS_SUCCESS on success.
+ *      - AGENTD_ERROR_DATASERVICE_RESPONSE_PACKET_INVALID_SIZE if the response
+ *        packet payload size is incorrect.
+ *      - AGENTD_ERROR_DATASERVICE_RESPONSE_INVALID_PARAMETER if one of the
+ *        parameters to the function is invalid.
+ */
+int dataservice_decode_response_block_get(
+    const void* resp, size_t size,
+    dataservice_response_block_get_t* dresp);
 
 /* make this header C++ friendly. */
 #ifdef __cplusplus
