44b1023c3b72942aa59e32b3aee37256b6cdc11d
Justin Handville <nanolith@gmail.com>
2020-04-23 02:13:20 -0400

    Add block read capability to peers.

--- src/protocolservice/unauthorized_protocol_service_dataservice_request_child_context.c
+++ src/protocolservice/unauthorized_protocol_service_dataservice_request_child_context.c
@@ -42,6 +42,8 @@ void unauthorized_protocol_service_dataservice_request_child_context(
     BITCAP_SET_TRUE(
         conn->dataservice_caps, DATASERVICE_API_CAP_APP_PQ_TRANSACTION_SUBMIT);
     BITCAP_SET_TRUE(
+        conn->dataservice_caps, DATASERVICE_API_CAP_APP_BLOCK_READ);
+    BITCAP_SET_TRUE(
         conn->dataservice_caps, DATASERVICE_API_CAP_LL_CHILD_CONTEXT_CLOSE);
 
     /*
